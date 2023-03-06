86940c61a07f7769b1c112b069a86b18f335c139
skymkmk <admin@skymkmk.com>
2023-03-06 18:03:20 +0800

    style: brand new home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            查看飞跃手册
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+      <main className={styles.main}>
+        <div className={`${styles.container} ${styles.titles}`}>
+          <div>
+            <div className={styles.h3}>安徽大学</div>
+            <div className={styles.h1}>飞跃手册</div>
+            <Link className="button button--secondary button--lg" to="/docs/intro">
+              查看飞跃手册
+            </Link>
+          </div>
+        </div>
+        <div className={styles.ahulogo}></div>
+      </main>
+    </Layout>
+  );
+}
